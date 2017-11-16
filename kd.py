e77a7675bd2d08d2f2fae2d6b38d8fdfe564f895
Anubhav Ashok <anubhava@andrew.cmu.edu>
2017-11-16 18:35:28 -0500

    Updated to the right subset

--- kd.py
+++ kd.py
@@ -2,13 +2,15 @@ from utils import *
 from datasets import mnist as dataset 
 from copy import deepcopy
 from models import conv3
+from models import conv4
 import torch
 
 torch.set_num_threads(4)
 
 teacher = torch.load('models/conv4.net').cuda()
+#teacher = torch.load('models/resnet18.net').cuda()
 # Currently using unmodified architecture (10 outputs)
-student = conv3.CONV3()
+student = conv4.CONV4()
 #deepcopy(teacher).cuda()
 resetModel(student)
 # Things to try:
@@ -16,10 +18,10 @@ resetModel(student)
 # Match logits at Dropout layer
 # Match logits at multiple places
 
-subset = [9, 8, 7, 6, 5]#[0, 1, 2, 3, 4]
+subset = [0, 1, 2, 3, 4]
 create_subset(subset, dataset)
 dataset.net = student.cuda()
-for i in range(1, 10):
-    dataset.train(i); dataset.test()
+#for i in range(1, 10):
+#    dataset.train(i); dataset.test()
 #trainTeacherStudent(teacher, student, dataset, epochs=30, lr=0.001)
-#trainTeacherStudentIntermediate(teacher, student, dataset, epochs=30, lr=0.001)
+trainTeacherStudentIntermediate(teacher, student, dataset, epochs=30, lr=0.001)
